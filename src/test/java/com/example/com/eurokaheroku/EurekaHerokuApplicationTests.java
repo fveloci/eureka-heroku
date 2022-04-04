57067fd36163d37a2cd40d7ae713573f1a21db4d
fveloci <fveloci@hotmail.com>
2022-04-04 17:24:25 -0300

    first commit

---
+++ src/test/java/com/example/com/eurokaheroku/EurekaHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.com.eurokaheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
